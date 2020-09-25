28caeca1e97726841aa1e7dfb66efb1cfd2c2bdb
Matt Isaacson <misaacson01@gmail.com>
2020-09-25 10:36:01 -0400

    Added support for non-scanimage .tif files; Added options to plot mean ROI intensity traces

--- support/read_tiffstate.m
+++ support/read_tiffstate.m
@@ -15,31 +15,37 @@ function out = read_tiffstate(tiff,statename)
 %tiff = Tiff('C:/image.tif','r');
 %fps = read_tiffstate(tiff,'framerate');
 
+out = nan;
+
 %get entire "ImageDescription" string using the matlab Tiff library
 try
     descr = getTag(tiff,'ImageDescription');
 catch
-    error('Could not get "ImageDescription" tag in Tiff file')
+    disp('Could not get "ImageDescription" tag in Tiff file');
+    return
 end
 
 %find the desired state
 name_ind = strfind(descr,statename);
-assert(~isempty(name_ind),['Could not find "' statename '" in Tiff file ImageDescription tag'])
+if isempty(name_ind)
+    disp(['Could not find "' statename '" in Tiff file ImageDescription tag'])
+    return
+end
 
 %find where the next state starts (i.e. the end of the desired state)
 end_ind = strfind(descr,'state');
 end_ind = end_ind(find(end_ind>name_ind,1));
 
-%get the string of the state field, using the appropraite delimiters
+%get the string of the state field with 2 possible delimiters
 state_str = descr(name_ind:end_ind-1);
 eq_ind = strfind(state_str,'=');
 ap_ind = strfind(state_str,'''');
 
 %return the state value as either a string or number
 if isempty(ap_ind)
-    out = str2num(state_str(eq_ind+1:end));
+    out = str2num(state_str(eq_ind+1:end)); %equal sign begins number
 else
-    out = state_str(ap_ind(1)+1:ap_ind(2)-1);
+    out = state_str(ap_ind(1)+1:ap_ind(2)-1); %apostrophes surround string
 end
 
 end
